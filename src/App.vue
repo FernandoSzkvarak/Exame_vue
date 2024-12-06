c0b11c82a495b570c804e7d70fc3328ae3361064
FernandoSzkvarak <fernandodesengrini11@gmail.com>
2024-12-05 21:03:33 -0300

    exame-vue-commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <CarComponent />
+  </div>
+</template>
+
+<script setup>
+import CarComponent from './components/CarComponent.vue';
+</script>
+
+<style>
+body {
+  font-family: 'Arial', sans-serif;
+  background-color: #f4f4f4;
+  color: #333;
+}
+</style>
